84429db4ca89c2511fcf886f73b6595d6198982f
wexeee <william.clarke@ndcn.ox.ac.uk>
2020-05-25 09:04:57 +0100

    Changes for pypi deployment.

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 with open('requirements.txt', 'rt') as f:
     install_requires = [l.strip() for l in f.readlines()]
 
 
-setup(name='pyMapVBVD',
-      version='0.2.0',
-      description='Python twix reader',
-      author=['Will Clarke'],
-      author_email=['william.clarke@ndcn.ox.ac.uk'],
-      url='https://github.com/wexeee/pymapvbvd',
-      packages=['mapVBVD'],
-      install_requires=install_requires         
+setuptools.setup(name='pyMapVBVD',
+        version='0.2.0',
+        description='Python twix file reader',
+        author='Will Clarke',
+        author_email='william.clarke@ndcn.ox.ac.uk',
+        url='https://github.com/wexeee/pymapvbvd',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        packages=setuptools.find_packages(),
+        install_requires=install_requires,
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.7',         
      )
