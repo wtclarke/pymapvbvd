cfcf3bd10bfdf6d70967cba4aff6e8528e6016a3
shahdloo <shahdloo@gmail.com>
2020-09-22 23:25:19 +0100

    tests updated

--- tests/test_flags.py
+++ tests/test_flags.py
@@ -9,9 +9,31 @@ from core import mapVBVD
 
 test_data_vb_broken = op.join(op.dirname(__file__), 'test_data', 'meas_MID111_sLaser_broken_FID4873.dat')
 test_data_gre = op.join(op.dirname(__file__), 'test_data', 'meas_MID00058_FID12358_gre_3D.dat')
+test_data_epi = op.join(op.dirname(__file__), 'test_data', 'meas_MID00053_FID12353_ep2d_QA_2mm_15ch_inplane_Grappa2_100.dat')
 
 def test_flagRemoveOS():
     twixObj = mapVBVD(test_data_gre, quiet=False)
     twixObj[1].image.flagRemoveOS = False
+    assert np.allclose(twixObj[1].image.dataSize, [256, 15, 128, 96, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    twixObj[1].image.flagRemoveOS = True
+    assert np.allclose(twixObj[1].image.dataSize, [128, 15, 128, 96, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 
-    assert np.allclose(twixObj.image.fullSize, [4096, 32, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1])
+    # broken file
+    twixObj = mapVBVD(test_data_vb_broken, quiet=False)
+    twixObj.image.flagRemoveOS = False
+    assert np.allclose(twixObj.image.dataSize, [4096, 32, 1, 1, 1, 1, 1, 1, 1, 97, 1, 1, 1, 1, 1, 1])
+    twixObj.image.flagRemoveOS = True
+    assert np.allclose(twixObj.image.dataSize, [2048, 32, 1, 1, 1, 1, 1, 1, 1, 97, 1, 1, 1, 1, 1, 1])
+
+def test_flagIgnoreSeg_flagDoAverage():
+    twixObj = mapVBVD(test_data_epi, quiet=False)
+    twixObj[1].image.flagIgnoreSeg = False
+    assert np.allclose(twixObj[1].image.dataSize, [86, 15, 86, 1, 36, 1, 1, 1, 100, 1, 2, 1, 1, 1, 1, 1])
+    twixObj[1].image.flagIgnoreSeg = True
+    assert np.allclose(twixObj[1].image.dataSize, [86, 15, 86, 1, 36, 1, 1, 1, 100, 1, 1, 1, 1, 1, 1, 1])
+
+    twixObj[1].refscanPC.flagDoAverage = False
+    assert np.allclose(twixObj[1].refscanPC.dataSize, [86, 15, 86, 1, 36, 1, 1, 1, 100, 1, 2, 1, 1, 1, 1, 1])
+    twixObj[1].refscanPC.flagDoAverage = True
+    assert np.allclose(twixObj[1].refscanPC.dataSize, [86, 15, 86, 1, 36, 1, 1, 1, 100, 1, 1, 1, 1, 1, 1, 1])
+
