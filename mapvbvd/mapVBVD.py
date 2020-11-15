c42341d04377f2e084fa1afc1fadbffd9793b942
shahdloo <shahdloo@gmail.com>
2020-11-15 16:37:47 +0000

    cleanup progressbar

--- mapvbvd/mapVBVD.py
+++ mapvbvd/mapVBVD.py
@@ -70,7 +70,7 @@ def loop_mdh_read(fid, version, Nscans, scan, measOffset, measLength, print_prog
     if print_prog:
         last_progress = 0
         t = tqdm(total=measLength, unit='B', unit_scale=True, unit_divisor=1024,
-                 desc='Scan %d/%d, read all mdhs' % (scan + 1, Nscans), leave=True)
+                 desc='Scan %d/%d, read all mdhs' % (scan + 1, Nscans), leave=False)
     while True:
         #         Read mdh as binary (uint8) and evaluate as little as possible to know...
         #           ... where the next mdh is (ulDMALength / ushSamplesInScan & ushUsedChannels)
