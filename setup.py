1a05ad3b0404c8473e51df7d03e2da42e2f0f1ec
wexeee <william.clarke@ndcn.ox.ac.uk>
2022-01-15 22:17:20 +0000

    Add tests to github CI.

--- setup.py
+++ setup.py
@@ -2,30 +2,33 @@
 
 import setuptools
 import versioneer
+import yaml
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt', 'rt') as f:
-    install_requires = [l.strip() for l in f.readlines()]
+with open("requirements.yml", "r") as stream:
+    try:
+        requirements = yaml.safe_load(stream)
+        install_requires = requirements['dependencies']
+    except yaml.YAMLError as exc:
+        print(exc)
 
-
-setuptools.setup(name='pyMapVBVD',
-        version=versioneer.get_version(),
-        cmdclass=versioneer.get_cmdclass(),
-        description='Python twix file reader',
-        author='Will Clarke',
-        author_email='william.clarke@ndcn.ox.ac.uk',
-        url='https://github.com/wexeee/pymapvbvd',
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        packages=setuptools.find_packages(),
-        install_requires=install_requires,
-        license_file='LICENSE',
-        classifiers=[
+setuptools.setup(
+    name='pyMapVBVD',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Python twix file reader',
+    author='Will Clarke',
+    author_email='william.clarke@ndcn.ox.ac.uk',
+    url='https://github.com/wexeee/pymapvbvd',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    license_file='LICENSE',
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        ],
-        python_requires='>=3.6',
-     )
+        "Operating System :: OS Independent", ],
+    python_requires='>=3.6')
