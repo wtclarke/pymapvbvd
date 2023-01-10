762173e77ea79d983d0fc9b600835efc354bf9a3
William T Clarke <william.clarke@ndcn.ox.ac.uk>
2023-01-10 10:34:47 +0000

    Typing hints requires 3.9

--- mapvbvd/mapVBVD.py
+++ mapvbvd/mapVBVD.py
@@ -247,7 +247,7 @@ def evalMDH(mdh_blob, version):
 
     evalInfoMask1 = mdh.aulEvalInfoMask[:, 0]
 
-    if version_info[1] > 7:
+    if version_info[1] > 8:
         mdh_type = np.ndarray[int, np.uint32]
     else:
         mdh_type = np.ndarray
