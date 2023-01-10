5890578ff2850fa32363c7e4027edfd6eef3ce28
William T Clarke <william.clarke@ndcn.ox.ac.uk>
2023-01-10 11:00:44 +0000

    Accidental repeat of python_requires

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=install_requires,
-    python_requires=">=3.7",
     license_file='LICENSE',
     classifiers=[
         "Programming Language :: Python :: 3",
