06404ec894e5ea8023be28d3d8ddb4ebd481e00a
William T Clarke <william.clarke@ndcn.ox.ac.uk>
2023-01-10 10:51:12 +0000

    Prepare for release

--- setup.py
+++ setup.py
@@ -27,9 +27,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=install_requires,
+    python_requires=">=3.7",
     license_file='LICENSE',
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent", ],
     python_requires='>=3.7')
