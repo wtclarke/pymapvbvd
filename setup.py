d6e601a1b94d105654462b17c31c1dba0d358dff
wtclarke <william.clarke@ndcn.ox.ac.uk>
2023-10-10 19:44:24 +0100

    Remove support and testing for python 3.7

--- setup.py
+++ setup.py
@@ -31,11 +31,10 @@ setuptools.setup(
     license_file='LICENSE',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent", ],
-    python_requires='>=3.7')
+    python_requires='>=3.8')
